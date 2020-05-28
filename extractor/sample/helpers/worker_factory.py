6d3de759f01653b91c9ef038e7ffca315aa4089b
Isaias Ramirez <soote1.ir@gmail.com>
2020-05-28 14:16:51 -0500

    added new helper to create worker instances

---
+++ extractor/sample/helpers/worker_factory.py
@@ -0,0 +1,12 @@
+import importlib
+
+class WorkerFactory(object):
+    @staticmethod
+    def create_instance(module_str, class_name_str):
+        """
+        Creates a new instance of a given class from a given module
+        """
+        class_module = importlib.import_module(module_str)
+        class_object = getattr(class_module, class_name_str)
+        
+        return class_object()
