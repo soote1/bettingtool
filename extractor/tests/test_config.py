032da73874ad7a0321826e34df4753ab5e27d310
Isaias Ramirez <soote1.ir@gmail.com>
2020-05-29 13:17:59 -0500

    wrapping up config tests in a class

--- extractor/tests/test_config.py
+++ extractor/tests/test_config.py
@@ -2,19 +2,29 @@ import pytest
 import os
 from extractor.sample.helpers.config import Config
 
-def test_should_create_config_dict():
-    expected_output = {
-        "producers":"",
-        "consumers": "",
-        "fetchers":"",
-        "seeders":[["extractor.sample.model.caliente_seeder", "CalienteSeeder"]],
-        "listeners":""
-    }
+class TestConfig:
+    def test_create_config_dict(self):
+        expected_output = {
+            "producers":"",
+            "consumers": "",
+            "fetchers":"",
+            "seeders":[["extractor.sample.model.caliente_seeder", "CalienteSeeder"]],
+            "listeners":""
+        }
 
-    with open("/home/soote1/projects/bettingtool/extractor/sample/config.json") as file:
-        config_str = file.read()
-        
-    config = Config(config_str)
+        with open("/home/soote1/projects/bettingtool/extractor/sample/config.json") as file:
+            config_str = file.read()
+            
+        config = Config(config_str)
 
-    assert expected_output == config.config
+        assert expected_output == config.config
 
+    def test_get_value_from_config(self):
+        expected_output = [["extractor.sample.model.caliente_seeder", "CalienteSeeder"]]
+
+        with open("/home/soote1/projects/bettingtool/extractor/sample/config.json") as file:
+            config_str = file.read()
+            
+        config = Config(config_str)
+        output = config.get("seeders")
+        assert expected_output == output
