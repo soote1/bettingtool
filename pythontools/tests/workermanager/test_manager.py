520cce10c2fa30591ebc03b462583f31e79d4c39
Isaias Ramirez <soote1.ir@gmail.com>
2020-06-18 09:58:53 -0500

    refactor tests to remove duplicate code

--- pythontools/tests/workermanager/test_manager.py
+++ pythontools/tests/workermanager/test_manager.py
@@ -4,40 +4,51 @@ from pythontools.workermanager.manager import WorkerManager
 from pythontools.tests.conftest import WorkerMock
 
 class TestWorkerManager:
+    """
+    Tests worker manager's core functionalities
+    """
+    def get_manager_and_workers(self, worker_manager_config):
+        worker_manager = WorkerManager(worker_manager_config)
+        return (worker_manager, worker_manager.create_workers(worker_manager.workers_config_list))
+
+    def get_manager_and_processes(self, worker_manager_config):
+        worker_manager, workers = self.get_manager_and_workers(worker_manager_config)
+        processes = worker_manager.create_processes(workers)
+        return (worker_manager, processes)
+
+    def create_and_start_processes(self, worker_manager_config):
+        worker_manager, processes = self.get_manager_and_processes(worker_manager_config)
+        worker_manager.start_processes(processes)
+
+        return (worker_manager, processes)
+
+    def validate_proccesses_are_alive(self, worker_manager, processes):
+        for process in processes:
+            assert process.is_alive()
+        worker_manager.stop_processes(processes)
+
     def test_create_and_load_config(self, worker_manager_config):
         worker_manager = WorkerManager(worker_manager_config)
         assert worker_manager.wait_time == worker_manager_config["wait_time"]
         assert worker_manager.workers_config_list == worker_manager_config["workers"]
 
     def test_create_workers(self, worker_manager_config):
-        worker_manager = WorkerManager(worker_manager_config)
-        workers = worker_manager.create_workers(worker_manager.workers_config_list)
+        workers = self.get_manager_and_workers(worker_manager_config)[1]
         for worker in workers:
             assert isinstance(worker, WorkerMock)
 
     def test_create_processes(self, worker_manager_config):
-        worker_manager = WorkerManager(worker_manager_config)
-        workers = worker_manager.create_workers(worker_manager.workers_config_list)
-        processes = worker_manager.create_processes(workers)
+        processes = self.get_manager_and_processes(worker_manager_config)[1]
         assert len(processes) == len(worker_manager_config["workers"])
         for process in processes:
             assert isinstance(process, Process)
 
     def test_start_processes(self, worker_manager_config):
-        worker_manager = WorkerManager(worker_manager_config)
-        workers = worker_manager.create_workers(worker_manager.workers_config_list)
-        processes = worker_manager.create_processes(workers)
-        worker_manager.start_processes(processes)
-        for process in processes:
-            assert process.is_alive()
+        worker_manager, processes = self.create_and_start_processes(worker_manager_config)
+        self.validate_proccesses_are_alive(worker_manager, processes)
 
     def test_stop_processes(self, worker_manager_config):
-        worker_manager = WorkerManager(worker_manager_config)
-        workers = worker_manager.create_workers(worker_manager.workers_config_list)
-        processes = worker_manager.create_processes(workers)
-        worker_manager.start_processes(processes)
-        for process in processes:
-            assert process.is_alive()
-        worker_manager.stop_processes(processes)
+        worker_manager, processes = self.create_and_start_processes(worker_manager_config)
+        self.validate_proccesses_are_alive(worker_manager, processes)
         for process in processes:
             assert not process.is_alive()
