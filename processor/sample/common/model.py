bb7449fd70b0b7e939a51f8ec814399908b8afe2
Isaias Ramirez <soote1.ir@gmail.com>
2020-06-17 19:13:45 -0500

    base worker classes added

---
+++ processor/sample/common/model.py
@@ -0,0 +1,44 @@
+class Worker:
+    """
+    Abstract class to define the basic structure of 
+    a specif type of worker.
+    """
+    def __init__(self):
+        """
+        Initialize worker instance.
+        """
+        raise NotImplementedError
+
+    def run(self):
+        """
+        Logic to be executed on each iteration.
+        """
+        raise NotImplementedError
+    
+    def do_work(self):
+        """
+        Performs a specific type of work.
+        """
+        raise NotImplementedError
+
+class TimedWorker(Worker):
+    def __init__(self, wait_time):
+        """
+        Initialize timed worker class.
+        """
+        self.wait_time = wait_time
+
+    def run(self, shutdown_event):
+        """
+        Calls do_work method inside an infinite loop and
+        wait n seconds to receive a shutdown event before
+        calling do_work again.
+        """
+        do_work = True
+        while not shutdown_event.is_set():
+            try:
+                if do_work:
+                    self.do_work()
+                shutdown_event.wait(self.wait_time)
+            except KeyboardInterrupt as error:
+                do_work = False
