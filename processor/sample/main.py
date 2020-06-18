2e5f678def8a02ee92be272a4a23eaa0d00de6fb
Isaias Ramirez <soote1.ir@gmail.com>
2020-06-17 19:15:16 -0500

    startup script added

--- processor/sample/main.py
+++ processor/sample/main.py
@@ -1,11 +1,6 @@
 import os
-import json
 
-from processor.sample.actionmanager.manager import ActionManager
+from processor.sample.workermanager.manager import WorkerManager
 
-with open(f"{os.path.dirname(__file__)}/config.json") as file:
-    config_str = file.read()
-
-config_dict = json.loads(config_str)
-action_manager = ActionManager(config_dict["arbitrage_betting_processor"])
-success = action_manager.run_workflow({"some_key":"some_value"})
+worker_manager = WorkerManager(f"{os.path.dirname(__file__)}/config.json")
+worker_manager.run()
