0168344e359b9b1ba0aba357e7653fa0ff2dbeed
Isaias Ramirez <soote1.ir@gmail.com>
2020-06-17 19:12:07 -0500

    arbitrage betting package created

--- processor/sample/actionmanager/actions.py
+++ processor/sample/actionmanager/actions.py
@@ -10,23 +10,3 @@ class BaseAction:
     
     def get_next_action(self):
         return self.config["next_action"]
-
-class DuplicateFilter(BaseAction):
-    """
-    An action to filter duplicate odds
-    """
-    def __init__(self, config):
-        super().__init__(config)
-
-    def run(self, data):
-        return data
-
-class IncompleteScoresFilter(BaseAction):
-    """
-    An action to filter filter games with incomplete scores
-    """
-    def __init__(self, config):
-        super().__init__(config)
-
-    def run(self, data):
-        return data
