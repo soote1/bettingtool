4568f57e8518b4e7382932105db405b0a399a829
Isaias Ramirez <soote1.ir@gmail.com>
2020-06-19 21:34:29 -0500

    configure loggin from main script

--- processor/sample/main.py
+++ processor/sample/main.py
@@ -1,7 +1,10 @@
 import os
 import json
+from multiprocessing import log_to_stderr
 
 from pythontools.workermanager.manager import WorkerManager
+
+log_to_stderr()
 
 with open(f"{os.path.dirname(__file__)}/config.json") as file:
     config = json.loads(file.read())
