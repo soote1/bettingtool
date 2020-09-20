ef44459aee97729f2f072fe7f8a528ef483f34f2
Isaias Ramirez <soote1.ir@gmail.com>
2020-09-20 12:59:41 -0500

    update arbs-stream service so that it connects to the right host and port

--- dashboard/src/app/features/arbs/services/arbs-stream/arbs-stream.service.ts
+++ dashboard/src/app/features/arbs/services/arbs-stream/arbs-stream.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ArbsStreamService {
-  url = 'http://172.22.145.122:1234/arbs';
+  url = 'http://localhost:3000/arbs';
   event = 'arb-found';
 
   constructor(private sseService: SseService) { }
